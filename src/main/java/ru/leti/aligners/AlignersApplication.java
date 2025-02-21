4113c6d1842a59149fda267fbc28230246c83a55
ForthA <tarasov02-03@mail.ru>
2025-02-21 15:53:10 +0300

    init proj

---
+++ src/main/java/ru/leti/aligners/AlignersApplication.java
@@ -0,0 +1,13 @@
+package ru.leti.aligners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlignersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlignersApplication.class, args);
+	}
+
+}

a75b722303e3932c7923c554e86497611979e382
ForthA <tarasov02-03@mail.ru>
2025-03-05 10:23:42 +0300

    init grpc

--- src/main/java/ru/etu/controlservice/service/PacsService.java
+++ src/main/java/ru/etu/controlservice/service/PacsService.java
@@ -2,6 +2,7 @@ package ru.etu.controlservice.service;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -9,21 +10,23 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
+import ru.etu.controlservice.CtMaskGrpc;
+import ru.etu.controlservice.CtMaskServiceProto;
 import ru.etu.controlservice.dto.DicomDto;
-import ru.etu.controlservice.dto.PacsZipCreationDto;
 import ru.etu.controlservice.dto.PacsZipCreationRequestDto;
 import ru.etu.controlservice.exceptions.PacsOperationException;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
 @Service
 public class PacsService {
+
+    @GrpcClient("CtMaskClient")
+    private CtMaskGrpc.CtMaskBlockingStub ctMaskBlockingStub;
 
     private final RestClient restClient;
 
@@ -53,8 +56,8 @@ public class PacsService {
 
     public List<DicomDto> sendInstance(MultipartFile file) {
         List<DicomDto> responses = new ArrayList<>();
-        try {
-            String response = restClient.post()
+        //try {
+            /*String response = restClient.post()
                     .uri(pacsBase + "/instances")
                     .contentType(MediaType.IMAGE_JPEG)
                     .body(file.getBytes())
@@ -62,11 +65,15 @@ public class PacsService {
                     .body(String.class);
             Type listType = new TypeToken<ArrayList<DicomDto>>(){}.getType();
             responses.addAll(new Gson().fromJson(response, listType));
+
+             */
+            String maskUrl = sendToPlug("responses.stream().findAny().get().parentStudy()");
+            System.out.println(maskUrl);
             //TODO: add study ID to table
             return responses;
-        } catch (IOException e) {
-            throw new PacsOperationException("Can't send request to PACS server: " + file.getOriginalFilename() + " was not uploaded", e);
-        }
+        //} catch (IOException e) {
+        //    throw new PacsOperationException("Can't send request to PACS server: " + file.getOriginalFilename() + " was not uploaded", e);
+       // }
     }
 
     public File getInstance(String id) {
@@ -88,6 +95,16 @@ public class PacsService {
                 .retrieve()
                 .body(String.class);
         return file.getBytes();
+    }
+
+    private String sendToPlug(String url){
+        CtMaskServiceProto.UrlRequest request = CtMaskServiceProto.UrlRequest.newBuilder()
+                .setUrl(url)
+                .build();
+
+        CtMaskServiceProto.UrlReply reply = ctMaskBlockingStub.sendDicomUrl(request);
+
+        return reply.getUrl();
     }
 
 }

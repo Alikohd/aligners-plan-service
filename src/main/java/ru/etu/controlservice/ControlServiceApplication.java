73676aef72be012267cc28c856a860170734d045
Alekhd <90794297+Alikohd@users.noreply.github.com>
2025-03-07 01:50:49 +0300

    feat: add ResultPlanning grpc client

--- src/main/java/ru/etu/controlservice/ControlServiceApplication.java
+++ src/main/java/ru/etu/controlservice/ControlServiceApplication.java
@@ -1,7 +1,14 @@
 package ru.etu.controlservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import ru.etu.controlservice.service.ResultPlanningClient;
+import ru.etu.grpc.resultplanning.AnatomicalStructure;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ControlServiceApplication {
@@ -10,4 +17,19 @@ public class ControlServiceApplication {
         SpringApplication.run(ControlServiceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(ResultPlanningClient client) {
+        return args -> {
+            // Тестовые данные
+            AnatomicalStructure structure = AnatomicalStructure.newBuilder()
+                    .setStl("s3://test.stl")
+                    .setRotationMatrix("[[1,0,0],[0,1,0],[0,0,1]]")
+                    .build();
+
+            // Используем бин
+            System.out.println("Sending request...");
+            List<String> result = client.planResult(List.of(structure));
+            System.out.println("Result: " + result);
+        };
+    }
 }
